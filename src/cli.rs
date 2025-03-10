973b74642b7d59e4678906036451ebbeee593fd0
CordlessCoder <roman@vm.net.ua>
2025-03-10 18:06:56 +0000

    Add prefix to env vars

--- src/cli.rs
+++ src/cli.rs
@@ -5,10 +5,10 @@ use std::{ffi::OsString, num::NonZeroU16, path::PathBuf, sync::Arc, time::Durati
 #[derive(Debug, Parser)]
 #[command(version, about)]
 pub struct Cli {
-    #[arg(long = "monitor", short = 'm')]
+    #[arg(env = "ROBOTSSH_MONITOR_PORT", long = "monitor", short = 'm')]
     pub monitor_port: Option<NonZeroU16>,
 
-    #[arg(long = "echo", short = 'e')]
+    #[arg(env = "ROBOTSSH_ECHO_SERVER_PORT", long = "echo", short = 'e')]
     pub echo_port: Option<NonZeroU16>,
 
     /// Arguments passed to SSH
@@ -16,27 +16,27 @@ pub struct Cli {
     pub ssh_args: Vec<OsString>,
 
     /// The path to SSH executable
-    #[arg(env, long, default_value = "ssh")]
+    #[arg(env = "ROBOTSSH_SSH_PATH", long, default_value = "ssh")]
     pub ssh_path: PathBuf,
 
     /// time to "make it out of the gate"
-    #[arg(env, long, value_parser = humantime::parse_duration, default_value = "30s")]
+    #[arg(env = "ROBOTSSH_GATE_TIME", long, value_parser = humantime::parse_duration, default_value = "30s")]
     pub gate_time: Duration,
 
     /// how many times to run (default no limit)
-    #[arg(env, long)]
+    #[arg(env = "ROBOTSSH_MAX_START_COUNT", long)]
     pub max_start: Option<usize>,
 
     /// how many times to retry the connection without backing off
-    #[arg(env, long, default_value_t = 2)]
+    #[arg(env = "ROBOTSSH_BACKOFF_FAST_TRIES", long, default_value_t = 2)]
     pub backoff_fast_tries: u32,
 
     /// What should be the absolute maximum time between connection attempts
-    #[arg(env, long, value_parser = humantime::parse_duration, default_value = "1h")]
+    #[arg(env = "ROBOTSSH_BACKOFF_UPPER_BOUND", long, value_parser = humantime::parse_duration, default_value = "1h")]
     pub backoff_upper_bound: Duration,
 
     /// how long can the process/daemon live
-    #[arg(env, long, value_parser = humantime::parse_duration)]
+    #[arg(env = "ROBOTSSH_MAX_LIFETIME", long, value_parser = humantime::parse_duration)]
     pub max_lifetime: Option<Duration>,
 
     #[command(flatten)]
@@ -46,21 +46,21 @@ pub struct Cli {
 #[derive(Args, Debug, Clone)]
 pub struct ConnectionTesterOptions {
     /// Default connection poll time
-    #[arg(env, long, value_parser = humantime::parse_duration, default_value = "10m")]
+    #[arg(env = "ROBOTSSH_POLL_TIME", long, value_parser = humantime::parse_duration, default_value = "10m")]
     pub poll_time: Duration,
 
     /// Initial connection poll time
-    #[arg(env, long, value_parser = humantime::parse_duration, default_value = "10m")]
+    #[arg(env = "ROBOTSSH_FIRST_POLL_TIME", long, value_parser = humantime::parse_duration, default_value = "10m")]
     pub first_poll_time: Duration,
 
-    #[arg(env, long, default_value = "")]
+    #[arg(env = "ROBOTSSH_ECHO_MESSAGE", long, default_value = "")]
     pub echo_message: Arc<str>,
 
     /// how many times to retry the network connection when monitoring
-    #[arg(env, long, default_value_t = 3)]
+    #[arg(env = "ROBOTSSH_MAX_CONN_TRIES", long, default_value_t = 3)]
     pub max_conn_tries: usize,
 
     /// timeout on network data
-    #[arg(env, long, value_parser = humantime::parse_duration, default_value = "15s")]
+    #[arg(env = "ROBOTSSH_NET_TIMEOUT", long, value_parser = humantime::parse_duration, default_value = "15s")]
     pub net_timeout: Duration,
 }
